acddd31214db0857b2af16ca38ba43940a701b3c
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-22 23:33:25 -0800

    Refactored breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,23 @@
 const request = require("request");
-const args = process.argv.slice(2);
-const catBreed = args[0];
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + catBreed;
 
-const fetch = function(url) {
+const fetchBreedDescription = function(catBreed, callback) {
+  const urlStart = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  const url = urlStart + catBreed;
 
   request(url, (error, response, body) => {
     setTimeout(() => {
       const textObj = JSON.parse(body);
-      if (textObj.length === 0) { // If no results are returned
-        console.log("Invalid breed name");
-        return;
+      let description = "Error: Invalid query";
+      if (error) {
+        return callback(error, description);
       }
-      console.log(textObj[0].description);
+      if (textObj.length !== 0) {
+        description = textObj[0].description;
+      }
+
+      return callback(error, description);
     }, 1);
   });
 };
 
-fetch(url);
+module.exports = { fetchBreedDescription };
